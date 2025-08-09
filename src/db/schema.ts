88a8dcf44c3f2c56cd6e0ad1a4c0517e0230a8b7
Sidney <sidneyferreirabaptistajunior3@gmail.com>
2025-08-09 14:20:31 -0300

    chore: add drizzle set with initial schema; add seed script

---
+++ src/db/schema.ts
@@ -0,0 +1,57 @@
+import { relations } from "drizzle-orm";
+import { integer, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const userTable = pgTable("user", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: text().notNull(),
+});
+
+export const categoryTable = pgTable("category", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: text().notNull(),
+  slug: text().notNull().unique(),
+  createAt: timestamp("created_at").notNull().defaultNow(),
+});
+export const categoryRelations = relations(categoryTable, ({ many }) => ({
+  products: many(productTable),
+}));
+
+export const productTable = pgTable("products", {
+  id: uuid().primaryKey().defaultRandom(),
+  categoryId: uuid("category_id")
+    .notNull()
+    .references(() => categoryTable.id),
+  name: text().notNull(),
+  slug: text().notNull().unique(),
+  description: text().notNull(),
+  createAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const productRelations = relations(productTable, ({ one,many }) => ({
+    category: one(categoryTable, {
+      fields: [productTable.categoryId],
+      references: [categoryTable.id],
+    }),
+    variants:many(productVariantTable)
+  }));
+  
+
+export const productVariantTable = pgTable("product_variant", {
+  id: uuid().primaryKey().defaultRandom(),
+  productId: uuid("product_id")
+    .notNull()
+    .references(() => productTable.id),
+  name: text().notNull(),
+  color:text().notNull(),
+  slug: text().notNull().unique(),
+  priceInCents: integer("price_in_cents").notNull(),
+  imageUrl: text("image_url").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const productVariantRelations = relations(productVariantTable, ({ one }) => ({
+    category: one(productTable, {
+      fields: [productVariantTable.productId],
+      references: [productTable.id],
+    }),
+  }))
